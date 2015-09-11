33d23dec5924e33aa97b4f34a9cc361358bd6e0c
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-11 19:13:21 +0900

    Update hsv.cpp

--- opencv/image/hsv.cpp
+++ opencv/image/hsv.cpp
@@ -6,9 +6,9 @@ using namespace cv;
 int main(void)
 {
 	Mat hsv;
-  Mat rgb = imread("test.png");	    // 画像の取得
-  cvtColor(rgb, hsv, CV_RGB2HSV);   // RGBからHSVに変換
-  imshow("Show image", hsv);		    // 画像の表示
-  waitKey(0);						            // 入力待機
-  return 0;
+	Mat rgb = imread("test.png");	// 画像の取得
+	cvtColor(rgb, hsv, CV_RGB2HSV); // RGBからHSVに変換
+	imshow("Show image", hsv);	// 画像の表示
+	waitKey(0);			// 入力待機
+	return 0;
 }
