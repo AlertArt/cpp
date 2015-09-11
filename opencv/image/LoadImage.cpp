b0f48619c3ea5122aeebfe13b2d0c6c2e799023b
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-11 15:02:41 +0900

    Update LoadImage.cpp

--- opencv/image/LoadImage.cpp
+++ opencv/image/LoadImage.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 int main(int argc, char *argv[])
 {
 	Mat im = imread("test.jpg");	// カラー(RGBの3チャンネル)で読み込み	
-	Mat im = imread("test.jpg", 1);	// グレースケール(3チャンネル)で読み込み
-	Mat im = imread("test.jpg", 0);	// カラー(3チャンネル)で読み込み
+	Mat im = imread("test.jpg", 1);	// カラー(3チャンネル)で読み込み
+	Mat im = imread("test.jpg", 0);	// グレースケール(3チャンネル)で読み込み
 	Mat im = imread("test.jpg", -1);// カラー(RGBAの4チャンネル)で読み込み
 }
