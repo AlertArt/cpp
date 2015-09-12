ddf50145c797e9e2df304a6ef1b3be2d050eaeb0
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-13 00:36:58 +0900

    Update sobel.cpp

--- opencv/image/detect_edge/sobel.cpp
+++ opencv/image/detect_edge/sobel.cpp
@@ -9,7 +9,7 @@ int main (int argc, char **argv)
 	cvtColor(im, gray, CV_BGR2GRAY);	// グレースケール変換
 	Sobel(gray, gray, CV_32F, 1, 1);	// Sobelフィルタでエッジ検出
 	convertScaleAbs(gray, gray, 1, 0);
-	imshow("Sobel", gray)			// ウィンドウに画像表示
-	waitKey(0)				// 任意のキーが押されるまで待機
+	imshow("Sobel", gray);			// ウィンドウに画像表示
+	waitKey(0);				// 任意のキーが押されるまで待機
 	return 0;
 }
