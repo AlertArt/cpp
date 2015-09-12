0deef6474c52c1b32efbde958a09136d3568bc88
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-13 01:47:52 +0900

    Update mask.cpp

--- opencv/image/mask.cpp
+++ opencv/image/mask.cpp
@@ -7,7 +7,7 @@ int main (int argc, char **argv)
 	Mat gray;
 	Mat im = imread("test.jpg");		// 入力画像を読み込み
 	Mat mask = imread("mask.jpg");		// マスク画像を読み込み
-    bitwise_and(im, im, mask);
+	bitwise_and(im, im, mask);
 	imshow("Result", im);		    	// ウィンドウに画像表示
 	waitKey(0);				            // 任意のキーが押されるまで待機
 	return 0;
