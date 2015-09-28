1204ecfc470d95e3ab40dffbfa8dacd03473c794
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-28 16:19:35 +0900

    Update Threshold.cpp

--- opencv/image/Threshold.cpp
+++ opencv/image/Threshold.cpp
@@ -4,13 +4,11 @@
 using namespace cv;
 
 int main(int argc, char* argv[]){
-	Mat gray, bin;					// 画像格納用
-  Mat im = imread("test.png");	// 画像の取得
-
-  if(!im.data) return -1;
+	Mat gray;						// 画像格納用
+	Mat im = imread("test.png");	// 画像の取得
 	cvtColor(im, gray, CV_RGB2GRAY);// グレースケール変換
-	threshold(gray, bin, 0, 255, cv::THRESH_BINARY|cv::THRESH_OTSU);	// 2値化
-  imshow("Show image", bin);		// 画像の表示
-  waitKey(0);						// 入力待機
-  return 0;
+	threshold(gray, gray, 0, 255, cv::THRESH_BINARY|cv::THRESH_OTSU);	// 2値化
+	imshow("Show image", gray);		// 画像の表示
+	waitKey(0);						// 入力待機
+	return 0;
 }
