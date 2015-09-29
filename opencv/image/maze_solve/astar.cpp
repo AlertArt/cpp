97b322b65b68c994e5801a2050a4af8756a6a277
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-29 23:35:59 +0900

    Update astar.cpp

--- opencv/image/maze_solve/astar.cpp
+++ opencv/image/maze_solve/astar.cpp
@@ -10,9 +10,9 @@ using namespace cv;
 using namespace std;
 const int w = 200, h = 200;			// 地図のサイズ
 static int MAP[w][h];
-static int closed_map[w][h];	// Closedノード用マップ
-static int open_map[w][h];	// Openノード用マップ
-static int dir_map[w][h];			// 方向用マップ
+static int closed_map[w][h];		// Closedノード用マップ
+static int open_map[w][h] = {0};	// Openノード用マップ
+static int dir_map[w][h] = {0};		// 方向用マップ
 const int dir = 8;					// 移動可能な方向数
 static int dx[dir] = { 1, 1, 0, -1, -1, -1, 0, 1 };
 static int dy[dir] = { 0, 1, 1, 1, 0, -1, -1, -1 };
@@ -48,8 +48,7 @@ public:
 	// ゴールまでの推定距離を求める関数
 	const int & estimate(const int & dx, const int & dy) const
 	{
-		static int xd, yd, d;
-		d = static_cast<int>(sqrt((dx - xp)*(dx - xp) + (dy - yp)*(dy - yp))); // ユークリッド距離
+		static int d = static_cast<int>(sqrt((dx - xp)*(dx - xp) + (dy - yp)*(dy - yp))); // ユークリッド距離
 		return(d);
 	}
 };
@@ -70,36 +69,23 @@ string pathFind(const int & x1, const int & y1, const int & x2, const int & y2)
 	static int i, j, x, y, xdx, ydy;
 	static char c;
 	pqi = 0;
-	// ノード用マップの初期化
-	for (y = 0; y<h; y++)
-	{
-		for (x = 0; x<w; x++)
-		{
-			closed_map[x][y] = 0;
-			open_map[x][y] = 0;
-		}
-	}
 	// 開始ノードを作成しOpenノードの配列に挿入
 	w0 = new node(x1, y1, 0, 0);
 	w0->updatePriority(x2, y2);
 	pq[pqi].push(*w0);
 	open_map[x][y] = w0->getPriority(); // Openノードマップ上にマーク
-
 	// A-Statで探索
-	while (!pq[pqi].empty())
-	{	// Openノードの配列から最も優先度の高い現在のノードを取得
+	while (!pq[pqi].empty()){
+		// Openノードの配列から最も優先度の高い現在のノードを取得
 		w0 = new node(pq[pqi].top().getxPos(), pq[pqi].top().getyPos(), pq[pqi].top().getLevel(), pq[pqi].top().getPriority());
 		x = w0->getxPos(); y = w0->getyPos();
-		pq[pqi].pop(); // オープンリストからノードを削除
+		pq[pqi].pop();				// オープンリストからノードを削除
 		open_map[x][y] = 0;
 		closed_map[x][y] = 1;		// 閉じたノードマップ上にマーク
-
 		// ゴールまで到達したら探索終了
-		if (x == x2 && y == y2)
-		{	// 求めた方向に従って経路を作成
+		if (x == x2 && y == y2){
 			string path = "";
-			while (!(x == x1 && y == y1))
-			{
+			while (!(x == x1 && y == y1)){
 				j = dir_map[x][y];
 				c = '0' + (j + dir / 2) % dir;
 				path = c + path;
@@ -110,41 +96,32 @@ string pathFind(const int & x1, const int & y1, const int & x2, const int & y2)
 			while (!pq[pqi].empty()) pq[pqi].pop();		// 残りのノードを空にする
 			return path;
 		}
-
 		// 移動可能な全方向に移動する子ノードを生成
-		for (i = 0; i<dir; i++)
-		{
+		for (i = 0; i<dir; i++){
 			xdx = x + dx[i]; ydy = y + dy[i];
-
-			if (!(xdx<0 || xdx>w - 1 || ydy<0 || ydy>h - 1 || MAP[xdx][ydy] == 1 || closed_map[xdx][ydy] == 1))
-			{	// 子ノードを作成
+			// 子ノードを作成
+			if (!(xdx<0 || xdx>w - 1 || ydy<0 || ydy>h - 1 || MAP[xdx][ydy] == 1 || closed_map[xdx][ydy] == 1)){
 				h0 = new node(xdx, ydy, w0->getLevel(), w0->getPriority());
 				h0->nextLevel(i);
 				h0->updatePriority(x2, y2);
 				// オープンリストにない場合は追加
-				if (open_map[xdx][ydy] == 0)
-				{
+				if (open_map[xdx][ydy] == 0){
 					open_map[xdx][ydy] = h0->getPriority();
 					pq[pqi].push(*h0);
-					dir_map[xdx][ydy] = (i + dir / 2) % dir;		// 親ノードの方向をマーク
+					dir_map[xdx][ydy] = (i + dir / 2) % dir;	// 親ノードの方向をマーク
 				}
-				else if (open_map[xdx][ydy]>h0->getPriority())
-				{
-					open_map[xdx][ydy] = h0->getPriority();	// 優先度の情報を更新
-					dir_map[xdx][ydy] = (i + dir / 2) % dir;		// 親ノードの情報を更新
+				else if (open_map[xdx][ydy]>h0->getPriority()){
+					open_map[xdx][ydy] = h0->getPriority();		// 優先度の情報を更新
+					dir_map[xdx][ydy] = (i + dir / 2) % dir;	// 親ノードの情報を更新
 
-					while (!(pq[pqi].top().getxPos() == xdx &&
-						pq[pqi].top().getyPos() == ydy))
-					{
+					while (!(pq[pqi].top().getxPos() == xdx && pq[pqi].top().getyPos() == ydy)){
 						pq[1 - pqi].push(pq[pqi].top());
 						pq[pqi].pop();
 					}
 					pq[pqi].pop();									// 希望のノードを削除
-
 					// より大きなサイズのPQを空にする
 					if (pq[pqi].size()>pq[1 - pqi].size()) pqi = 1 - pqi;
-					while (!pq[pqi].empty())
-					{
+					while (!pq[pqi].empty()){
 						pq[1 - pqi].push(pq[pqi].top());
 						pq[pqi].pop();
 					}
@@ -182,31 +159,20 @@ int main(int argc, char *argv[])
 	loadMap(im);
 	int x1 = 0, y1 = 0, x2 = 190, y2 = 190;
 	string route = pathFind(x1, y1, x2, y2);
-
-	if (route.length()>0)
-	{
-		int j; char c;
-		int x = x1;
-		int y = y1;
-		MAP[x][y] = 2;
-		for (int i = 0; i<route.length(); i++)
-		{
-			c = route.at(i);
-			j = atoi(&c);
-			x = x + dx[j];
-			y = y + dy[j];
-			MAP[x][y] = 3;
-		}
-		MAP[x][y] = 4;
+	// エラー処理(経路が見つからなかい場合)
+	if (route.length() == 0) return -1;
+	int j; char c;
+	int x = x1;
+	int y = y1;
+	for (int i = 0; i<route.length(); i++){
+		c = route.at(i);
+		j = atoi(&c);
+		x = x + dx[j];
+		y = y + dy[j];
 		// 算出した最短経路を地図画像に描く
-		for (int y = 0; y < h; y++)
-		{
-			for (int x = 0; x < w; x++)
-				if (MAP[x][y] == 3)
-					ellipse(im, Point(x, y), Size(1, 1), 0, 0, 360, Scalar(0, 0, 200));
-		}
+		ellipse(im, Point(x, y), Size(1, 1), 0, 0, 360, Scalar(0, 0, 200));
 	}
-	imwrite("map2.jpg",im);
+	imwrite("map2.jpg", im);
 	return(0);
 
 }
