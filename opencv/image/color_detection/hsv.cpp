a54d0959294cfb8c2b8931794af57f948c680ad0
tanaka0079 <yadoran001@yahoo.co.jp>
2015-10-04 16:43:38 +0900

    Update hsv.cpp

--- opencv/image/color_detection/hsv.cpp
+++ opencv/image/color_detection/hsv.cpp
@@ -6,8 +6,7 @@ using namespace cv;
 class Txt{
 private:
 public:
-	Txt(){
-	}
+	Txt(){}
 
 	double *x, *y, *z;
 	int lines;
@@ -18,8 +17,7 @@ public:
 		// ローカル変数の定義
 		FILE *fp;
 		// エラー処理
-		if ((fp = fopen(fname, "a")) == NULL)
-		{
+		if ((fp = fopen(fname, "a")) == NULL){
 			printf("FILE not open\n");
 			exit(1);
 		}
@@ -29,18 +27,17 @@ public:
 	}
 };
 
-
 void obstDetection(Mat im, char* fname){
-	Txt obst; // テキストファイルの処理
-	Mat hsv, mask;					        // 画像オブジェクトの宣言
-	cvtColor(im, hsv, CV_BGR2HSV);      // 画像をRGBからHSVに変換
+	Txt obst						// テキストファイルの処理
+	Mat hsv, mask;					// 画像オブジェクトの宣言
+	cvtColor(im, hsv, CV_BGR2HSV);	// 画像をRGBからHSVに変換
 	inRange(hsv, Scalar(160, 150, 0), Scalar(190, 255, 255), mask);	// 色検出でマスク画像の作成
 	int h = mask.rows;
 	int w = mask.cols;
 	for (int y = 0; y < h; y++){
 		for (int x = 0; x < w; x++){
 			if (mask.data[y * h + x] == 255){
-				obst.saveTxt(fname, x, y);
+				obst.saveTxt(fname, x, y);		// 赤色部分の座標をテキストファイルに保存
 			}
 		}
 	}
@@ -48,7 +45,7 @@ void obstDetection(Mat im, char* fname){
 
 int main(int argc, char *argv[])
 {
-	Mat im = imread("map.jpg");	// 画像の取得
+	Mat im = imread("map.jpg");		// 画像の取得
 	obstDetection(im, "obst.csv");
 	printf("finish");
 	return(0);
